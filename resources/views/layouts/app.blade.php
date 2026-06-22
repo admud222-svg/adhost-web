c50f751a18352927d18a4760d37b83c5464e3d47
admud222-svg <admud222-svg@users.noreply.github.com>
2026-06-23 03:00:10 +0700

    Force root URL from config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;450;500;600;700;800&family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ config('app.url') }}/css/style.css">
   @stack('styles')
 </head>
 <body>
@@ -69,7 +69,7 @@
     </div>
   </footer>
 
-  <script src="{{ asset('js/script.js') }}"></script>
+  <script src="{{ config('app.url') }}/js/script.js"></script>
   @stack('scripts')
 </body>
 </html>
